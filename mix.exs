af6d764253880a42d6f38af68233c6bc57f4be41
trojanh <rajan.tiwari@kiprosh.com>
2018-01-05 20:33:46 +0530

    update dependencies

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Tasker.Mixfile do
 
   defp deps do
     [
-      {:slack, "~> 0.8.0"},
+      {:plug, "~> 1.4.3"},
+      {:slack, "~> 0.12.0"},
       {:con_cache, "~> 0.11.1"},
       {:quantum, "~> 1.8.0"}
     ]
